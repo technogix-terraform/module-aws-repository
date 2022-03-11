7c76245898f9eff6b9400848166ff4ce2e164c79
Nadege LEMPERIERE <nadege.lemperiere@gmail.com>
2022-03-11 12:38:41 +0100

    feat/cicd : Correcting failing pipelines

--- scripts/robot.sh
+++ scripts/robot.sh
@@ -17,11 +17,17 @@
 script=$(readlink -f $0)
 scriptpath=`dirname $script`
 
-# Consider all additional arguments as a case to test (if none, all cases are tested)
-args=''
-for level in "$@"; do
-    export args=$args" --loglevel $level"
+# Parse arguments from flags
+args=""
+while getopts s:l:d: flag
+do
+    case "${flag}" in
+          s) args+=" --suite ${OPTARG}";;
+          l) args+=" --loglevel ${OPTARG}";;
+          d) args+=" --log ${OPTARG}/log.html --report ${OPTARG}/report.html";;
+    esac
 done
+
 
 # Install required python packages
 pip install --quiet --no-warn-script-location -r $scriptpath/../requirements-test.txt
